c295f639d9438159fbcff9618c89aae85b8ff382
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-05-11 23:51:42 +0530

    categories mapped to nav succesfully

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { ProductContext } from "../utils/Context";
+import { Link } from "react-router-dom";
 
 function Nav() {
   const [products] = useContext(ProductContext);
-  const distinct_category =
+  let distinct_category =
     products && products.reduce((acc, cv) => [...acc, cv.category], []);
   // we'llget all the categories, but they are repeted
-  distinct_category = [...new set(distinct_category)];
+  distinct_category = [...new Set(distinct_category)];
   // to make it distinct category array, we've used set. it is an inbuilt js class stores unique elements.
   console.log(distinct_category);
 
@@ -20,20 +21,18 @@ function Nav() {
       </a>
       <hr className="my-3 w-[80%]" />
       <h1 className="text-2xl mb-4 w-[80%] font-semibold">Category</h1>
-      <ul className=" w-[80%]">
-        <li className="flex items-center gap-2 mb-2">
-          <span className="rounded-full bg-blue-300 w-[15px] h-[15px]"></span>
-          Cat-1
-        </li>
-        <li className="flex items-center gap-2 mb-2">
-          <span className="rounded-full bg-green-300 w-[15px] h-[15px]"></span>
-          Cat-2
-        </li>
-        <li className="flex items-center gap-2 mb-2">
-          <span className="rounded-full bg-orange-300 w-[15px] h-[15px]"></span>
-          Cat-3
-        </li>
-      </ul>
+      <div className=" w-[80%]">
+        {distinct_category.map((item, index) => (
+          <Link
+            to={`/?category=${item}`}
+            key={index}
+            className="flex items-center gap-2 mb-2"
+          >
+            <span className="rounded-full bg-blue-300 w-[15px] h-[15px]"></span>
+            {item}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 }
