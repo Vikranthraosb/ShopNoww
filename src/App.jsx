32bbfe1e16590f07a416bdeb617653c991666048
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-05-14 23:07:56 +0530

    half project finished. backennd api is working fine. moving towords Edit/Delete Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import Details from "./components/Details";
-import Loading from "./components/Loading";
 
 function App() {
+  const { search, pathname } = useLocation();
   return (
     <div className="h-screen w-screen flex">
-      <Link to="/" className="text-xl text-red-500">
-        Home
-      </Link>
+      {(pathname != "/" || search.length > 0) && (
+        <Link
+          to="/"
+          className="text-xl text-red-500 absolute left-[17%] top-[1%] font-bold"
+        >
+          Home
+        </Link>
+      )}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:id" element={<Details />} />
